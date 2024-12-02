887843cfa219627959177b43efbb052fb6a4ad03
Aitor <aitorarceus2005@gmail.com>
2024-12-02 18:48:40 +0100

    README.java

--- README.java
+++ README.java
@@ -1,2 +1,13 @@
-# ProyectoEval1
-EXAMEN DE ENTORNOS
+package proyectoeval1;
+
+/**
+ *
+ * @author aitor
+ */
+public class ProyectoEval1 {
+
+    public static void main(String[] args) {
+        System.out.println("HolaMundo");
+    }
+    
+}
